12945997e137e7e0cca3e59127050a7af9287d0c
hiromi-mi <hiromi-mi@cat.zaq.jp>
2018-08-16 21:02:12 +0900

    Treat as Wide for UNICODE Support

--- oblaka.c
+++ oblaka.c
@@ -1,19 +1,23 @@
-// Author: hiromi-mi
+// Author: hiromi-mi <hiromi hyphen mi at cat dot zaq dot jp>
 //
 //正規表現エンジンもどきの何か
 
+#define _XOPEN_SOURCE
+#include <locale.h>
 #include <stdio.h>
+#include <wchar.h>
+#include <stdlib.h>
 
-int match_here(char *pattern, char *regexp);
+int match_here(wchar_t *pattern, wchar_t *regexp);
 
-int match_str(char c, char *pattern, char *regexp) {
+int match_str(wchar_t c, wchar_t *pattern, wchar_t *regexp) {
    while (*pattern == c) {
       pattern++;
    }
    return match_here(pattern, regexp);
 }
 
-int match_here(char *pattern, char *regexp) {
+int match_here(wchar_t *pattern, wchar_t *regexp) {
    if (*regexp == '\0' || *regexp == '$') {
       return 1;
    }
@@ -32,7 +36,7 @@ int match_here(char *pattern, char *regexp) {
    return 0;
 }
 
-int match(char *pattern, char *regexp) {
+int match(wchar_t *pattern, wchar_t *regexp) {
    if (*regexp == '^') {
       return match_here(pattern, regexp+1);
    }
@@ -45,19 +49,21 @@ int match(char *pattern, char *regexp) {
 }
 
 int main() {
-   char pattern[256], regexp[256];
+   char pat[256], reg[256];
+   wchar_t pattern[256], regexp[256];
    int i;
+   setlocale(LC_ALL, "");
    printf("Pattern: ");
-   if (fgets(pattern,256 ,stdin) == NULL) {
+   if (fgets(pat,256 ,stdin) == NULL) {
       return -1;
    }
    printf("Regexp: ");
-   if (fgets(regexp, 256 ,stdin) == NULL) {
+   if (fgets(reg, 256 ,stdin) == NULL) {
       return -1;
    }
    for (i=0;i<256;i++) {
-      if (pattern[i] == '\n') {
-	 pattern[i] = '\0';
+      if (pat[i] == '\n') {
+	 pat[i] = '\0';
 	 break;
       }
       if (i == 255) {
@@ -65,14 +71,16 @@ int main() {
       }
    }
    for (i=0;i<256;i++) {
-      if (regexp[i] == '\n') {
-	 regexp[i] = '\0';
+      if (reg[i] == '\n') {
+	 reg[i] = '\0';
 	 break;
       }
       if (i == 255) {
 	 while(getchar() != '\n');
       }
    }
+   mbstowcs(regexp, reg, 256);
+   mbstowcs(pattern, pat, 256);
 
    puts((match(pattern, regexp) == 1) ? "Matched" : "Not Matched");
    return 0;
