69a7c742434a571a70a17854aaf8fe3bb8931533
hiromi-mi <hiromi-mi@cat.zaq.jp>
2018-09-18 23:18:19 +0900

    iroiro

--- oblaka.h
+++ oblaka.h
@@ -9,7 +9,35 @@
 #define _XOPEN_SOURCE
 #include <locale.h>
 #include <stdlib.h>
+#include <malloc.h>
 #include <wchar.h>
+
+typedef enum TokenKind {
+   tSTRING,
+   tLEFTPARENSIS,
+   tRIGHTPARENSIS,
+   tSELECT
+};
+
+typedef struct {
+   TokenKind kind;
+   wchar_t *str;
+} Token;
+
+typedef struct {
+   wchar_t *buf;
+   size_t len;
+   size_t reserved_len;
+} WString;
+
+void *oblaka_safe_malloc(size_t size) {
+   return malloc(size);
+}
+
+void oblaka_safe_free(void *ptr) {
+   free(ptr);
+   ptr = NULL;
+}
 
 int match(wchar_t *pattern, wchar_t *regexp);
 int match_here(wchar_t *pattern, wchar_t *regexp, unsigned int *pat_pos,
@@ -66,58 +94,68 @@ int match_paren(wchar_t *pattern, wchar_t *regexp, unsigned int *pat_pos,
       // 最初のpos は ( から.
       return OBLAKA_NOMATCH;
    }
+}
 
-   int match_here(wchar_t * pattern, wchar_t * regexp, unsigned int *pat_pos,
-                  unsigned int *reg_pos) {
-      if (regexp[*reg_pos] == '\0' || regexp[*reg_pos] == '$') {
+int match_here(wchar_t *pattern, wchar_t *regexp, unsigned int *pat_pos,
+               unsigned int *reg_pos) {
+   if (regexp[*reg_pos] == '\0' || regexp[*reg_pos] == '$') {
+      return 1;
+   }
+   if (pattern[*pat_pos] == '\0') {
+      return OBLAKA_NOMATCH;
+   }
+   if (regexp[*reg_pos] == '*') {
+      return match_star(regexp, pattern, regexp + 2, pat_pos, reg_pos);
+   }
+
+   if (regexp[*reg_pos] == '\\') {
+      reg_pos++;
+   }
+
+   /*
+   //面倒なので後から
+   if (regexp[0] == '(') {
+   }
+   */
+
+   // マッチ
+   if (pattern[*pat_pos] == regexp[*reg_pos]) {
+      if (regexp[*reg_pos + 1] == '(' && regexp[*reg_pos] != '\\') {
+         // 繰り返しの場合
+         ++pat_pos;
+         ++reg_pos;
+         return match_paren(pattern, regexp, pat_pos, reg_pos);
+      } else {
+         ++pat_pos;
+         ++reg_pos;
+         return match_here(pattern, regexp, pat_pos, reg_pos);
+      }
+   }
+   return OBLAKA_NOMATCH;
+}
+
+void oblaka_tokenize_regexp(Token* tokens, const wchar_t *regexp) {
+}
+
+int match(const wchar_t *pattern, const wchar_t *regexp) {
+   /*
+   unsigned int pat_pos = 0;
+   unsigned int reg_pos = 0;
+   if (regexp[reg_pos] == '^') {
+      reg_pos++;
+      return match_here(pattern, regexp, &pat_pos, &reg_pos);
+   }
+   do {
+      if (match_here(pattern, regexp, &pat_pos, &reg_pos) == 1) {
          return 1;
       }
-      if (pattern[*pat_pos] == '\0') {
-         return OBLAKA_NOMATCH;
-      }
-      if (regexp[*reg_pos] == '*') {
-         return match_star(regexp, pattern, regexp + 2, pat_pos, reg_pos);
-      }
-
-      if (regexp[*reg_pos] == '\\') {
-         reg_pos++;
-      }
-
-      /*
-      //面倒なので後から
-      if (regexp[0] == '(') {
-      }
-      */
-
-      // マッチ
-      if (pattern[*pat_pos] == regexp[*reg_pos]) {
-         if (regexp[*reg_pos + 1] == '(' && regexp[*reg_pos] != '\\') {
-            // 繰り返しの場合
-            ++pat_pos;
-            ++reg_pos;
-            return match_paren(pattern, regexp, pat_pos, reg_pos);
-         } else {
-            ++pat_pos;
-            ++reg_pos;
-            return match_here(pattern, regexp, pat_pos, reg_pos);
-         }
-      }
-      return OBLAKA_NOMATCH;
-   }
-
-   int match(wchar_t * pattern, wchar_t * regexp) {
-      unsigned int pat_pos = 0;
-      unsigned int reg_pos = 0;
-      if (regexp[reg_pos] == '^') {
-         reg_pos++;
-         return match_here(pattern, regexp, &pat_pos, &reg_pos);
-      }
-      do {
-         if (match_here(pattern, regexp, &pat_pos, &reg_pos) == 1) {
-            return 1;
-         }
-      } while (pattern[pat_pos] != '\0');
-      return OBLAKA_NOMATCH;
-   }
+   } while (pattern[pat_pos] != '\0');
+   return OBLAKA_NOMATCH;
+   */
+   Token *tokens = oblaka_safe_malloc(sizeof(Token) * 128);
+   oblaka_tokenize_regexp(tokens, regexp);
+   oblaka_safe_free(tokens);
+   return OBLAKA_NOMATCH;
+}
 
 #endif
